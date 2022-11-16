577d2201cbaa3ae48c3d5c11fe109815ce096145
K Odewale <kayode.odewal1@nhs.net>
2022-11-16 18:25:56 +0000

    Downgraded to version 0.3.9

--- charts/deploy-services.sh
+++ charts/deploy-services.sh
@@ -1,3 +1,3 @@
-helmfile sync -n microservices-development
-kubectl apply -f loadgenerator.yaml -n microservices-development
 kubectl apply -f redis-cart.yaml -n microservices-development
+helmfile sync -n microservices-development
+#kubectl apply -f loadgenerator.yaml -n microservices-development
