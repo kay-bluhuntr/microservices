621ad08b981ed247156335c22871891519b750e1
kayode <61321539+kay-bluhuntr@users.noreply.github.com>
2024-08-08 20:41:28 +0100

    Removed data directory

--- infrastructure/data/modules/aurora-cluster-parameter/output.tf
+++
@@ -1,4 +0,0 @@
-output "aurora_cluster_parameter_group_name" {
-  description = "The parameter group"
-  value       = aws_rds_cluster_parameter_group.cluster_parameter_group.name
-}
--- infrastructure/data/modules/aurora-instance-parameter/instance-params.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_db_parameter_group" "aurora_instance_parameter_group" {
-  name = var.aurora_instance_parameter_group_name
-  family      = var.aurora_family
-  description = var.description
-
-}
